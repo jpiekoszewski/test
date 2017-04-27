7510fd3af34d34d695ce65e8dfd8d3db51dda2b9
jarek.piekoszewski <jarek.piekoszewski@ho-ltp-15809.Interlink.local>
2017-04-27 15:11:25 +0200

    Zalozenie projektu w Netbeansie

---
+++ J11-Git/src/hello/HelloJarek.java
@@ -0,0 +1,14 @@
+ 
+package hello;
+
+ 
+public class HelloJarek {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Jarek");
+        
+    }
+    
+    
+}
